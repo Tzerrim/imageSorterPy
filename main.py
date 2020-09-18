98ccba7908b60a3af313b650cd5be24f8fb9acc7
Tzerrim <shadowsirius@gmail.com>
2020-09-18 06:23:23 +0300

    filename key option

--- main.py
+++ main.py
@@ -27,9 +27,9 @@ def move_copy():
 def getFilenameFromSource():
 
     for file in os.listdir(source_dir):
-        print("Process file: ", file)
+        print("Process file: ", file, " ", file.lower().find(filename_key.lower()))
         if file.endswith(extensions):
-            if (filename_key != "" and file.find(filename_key)) or filename_key == "":
+            if (filename_key != "" and file.lower().find(filename_key.lower())) or filename_key == "":
                 file_names.append(file)
     print("Files to move: ", len(file_names))
 
@@ -47,6 +47,7 @@ parser = argparse.ArgumentParser()
 
 parser.add_argument('-s', action="store", dest="source_dir", default="")
 parser.add_argument('-d', action="store", dest="destination_dir", default="")
+parser.add_argument('-f', action="store", dest="filename_key", default="")
 parser.add_argument('-c', action="store", dest="pack_copy_flag", default=False)
 parser.add_argument('-p', action="store", dest="files_in_folders", default=0, type=int)
 
@@ -55,5 +56,6 @@ print(args)
 
 source_dir = args.source_dir
 destination_dir = args.destination_dir
+filename_key = args.filename_key
 
 move_copy()
