87ccb849b68e4694ec20f9f7e9b8d8777eb271d5
Tzerrim <shadowsirius@gmail.com>
2020-09-18 04:51:42 +0300

    init2

---
+++ main.py
@@ -0,0 +1,51 @@
+import os
+import argparse
+
+# Variables
+source_dir = ""
+destination_dir = ""
+filename_key = ""
+pack_in_folders_flag = False
+pack_copy_flag = False
+files_in_folders = 0
+file_names = []
+
+keys = ("-s", "-d", "-c", "-p") # Keys for arguments. -s - source, -d - destiantion, -c - copy, -p - pack
+extensions = (".png", ".jpeg", ".jpg", ".gif") # File extensions that will process this script
+
+def move_copy():
+    print("Moving files . . . ")
+    global source_dir
+    source_dir = endSlashAppender(source_dir)
+    for file in file_names:
+        source_file = source_dir+file
+        destination_file = destination_dir + file
+        print("Moving: ", source_file, " To: ", destination_file)
+        os.rename(source_file , destination_file)
+
+def getFilenameFromSource():
+
+    for file in os.listdir(source_dir):
+        if file.endswith(extensions) and file.find(filename_key):
+            file_names.append(file)
+    print("Files to move: ", len(file_names))
+
+def fileToFolderPacker():
+    pass;
+
+def endSlashAppender (path):
+    if not path.endswith("\\"):
+        path.join("\\")
+    return path
+
+
+# Parse arguments
+parser = argparse.ArgumentParser()
+
+parser.add_argument('-s', action="store", dest="source_dir", default="")
+parser.add_argument('-d', action="store", dest="destination_dir", default="")
+parser.add_argument('-c', action="store", dest="pack_copy_flag", default=False)
+parser.add_argument('-p', action="store", dest="files_in_folders", default=0, type=int)
+
+args = parser.parse_args()
+print(args)
